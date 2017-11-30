f265281ce574f88843e5003b760e0a5a8f18fc28
lukas correa <correa97lukas@hotmail.com>
2017-11-30 15:45:08 -0300

    structure class

--- example.php
+++ example.php
@@ -1,83 +1,123 @@
 <?php
 
-error_reporting(-1);
+/**
+* Copyright (C) 2017 Lucas Correa.
+*/
 
-try {
-    
-    if (@class_exists('COM')) {
-        echo "creando interface ...";
-        $ctrl = new COM('PyFiscalPrinter') or die("No se puede crear el objeto");
-        echo "interface creada version {$ctrl->Version}\n";
+class pyPrinter
+{
+
+  function __construct($env)
+  {
+    $this->Connect($env);
+  }
+
+  public function receipt()
+  {
+    try {
+      $ult = $this->ctrl->ConsultarUltNro($_POST['typeinvoice']);
+      $nro = "{$ult}\n"+1;
+
+      $tipo_cbte = $_POST['typeinvoice'];
+      $tipo_doc = 80;                   // CUIT
+      $nro_doc = $_POST['cuit'];
+      $nombre_cliente = $_POST['razonsocial'];
+      $domicilio_cliente = $_POST['domicilio'];
+      $tipo_responsable = $_POST['conditioniva'];
+      $referencia = "";                       // solo para NC / ND
+
+      $this->ctrl->AbrirComprobante($tipo_cbte, $tipo_responsable, 
+        $tipo_doc, $nro_doc,
+        $nombre_cliente, $domicilio_cliente, 
+        $referencia);            
+      
+      # Imprimo un art�culo:
+      $ds = $_POST['description'];
+      $qty = $_POST['quantity'];
+      $price = $_POST['price'];
+      $bonif = 0.00; 
+      $alic_iva = $_POST['typeiva'];
+      $type_payment = $_POST['type_payment'];
+      $payment = $_POST['payment'];
+
+      $this->ctrl->ImprimirItem($ds, $qty, $price, $alic_iva);
+
+          # Imprimir un pago (si es superior al total, se imprime el vuelto):
+      $this->ctrl->ImprimirPago($type_payment, $payment);
+
+          # Finalizar el comprobante (imprime pie del comprobante, CF DGI, etc.) 
+      $this->ctrl->CerrarComprobante();
+      echo "<h2> Su comprobante N° ".$nro." se imprimió con éxito!</h2>";
+
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+  }
+
+  private function Connect($env)
+  {
+    error_reporting(-1);
+    try {
+      if (@class_exists('COM')) {
+        $this->ctrl = new COM('PyFiscalPrinter') or die("No se puede crear el objeto");
         # habilitar excecpciones (capturarlas con un bloque try/except), ver abajo:
-        $ctrl->LanzarExcepciones = true;
-    } else if (@class_exists('Dbus')) {
-        $dbus = new Dbus( Dbus::BUS_SESSION, true );
-        $ctrl = $dbus->createProxy("ar.com.pyfiscalprinter.Service",  
-                                   "/ar/com/pyfiscalprinter/Object",  
-                                   "ar.com.pyfiscalprinter.Interface");
-    } else {
+        $this->ctrl->LanzarExcepciones = true;
+      } else if (@class_exists('Dbus')) {
+          $dbus = new Dbus( Dbus::BUS_SESSION, true );
+          $this->ctrl = $dbus->createProxy("ar.com.pyfiscalprinter.Service",  
+                                          "/ar/com/pyfiscalprinter/Object",  
+                                          "ar.com.pyfiscalprinter.Interface");
+      } else {
         echo "No existe soporte para COM (Windows) o DBus (Linux) \n";
         exit(1);
-    }
+      }
 
-    # Iniciar conexi�n con el controlador fiscal:
-    $marca = "epson";            // configurar "hasar" o "epson"
-    $modelo = "epsonlx300+";     // "tickeadoras", "epsonlx300+", "tm-220-af"
-                                 // "615", "715v1", "715v2", "320"
-    $puerto = "COM2";            // "COM1", "COM2", etc. o "/dev/ttyS0" en linux
-    $equipo = "";                // IP si no esta conectada a esta m�quina
-    $ok = $ctrl->Conectar($marca, $modelo, $puerto, $equipo);
-    
-    # Analizar errores (si no se habilito lanzar excepciones)
-    if (!$ok) {
+      $ok = $this->ctrl->Conectar($env['marca'], $env['modelo'], 
+        $env['puerto'], $env['equipo']);
+
+      if ($ok) {
+        echo "<h4>Impresora '".$env['marca'].' '.$env['modelo']."' conectada!</h4>";
+      } else {
+        # Analizar errores (si no se habilito lanzar excepciones)
         echo "Excepcion: {$ctrl->Excepcion}\n";
         echo "Traza: {$ctrl->Traceback}\n";
         exit(1);
+      }
+    } catch(Exception $e) {
+      echo $e->getMessage();
     }
+  }
 
-    # Consultar el �ltimo n�mero de comprobante impreso por el controlador:
-    # IMPORTANTE: en modo dummy solicita el n�mero de comprobante por consola
-    $tipo_cbte = 83;
-    $ult = $ctrl->ConsultarUltNro($tipo_cbte);
-    echo "Ultimo Nro de Cbte {$ult}\n";
+  public function closingZ() {
+    try {
+      if ($this->ctrl->CierreDiario('Z')) {
+        echo "<h3>Se realizó un cierre Z!</h3>";
+      }
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+  }
 
-    # Creo una factura de ejemplo:
-    $tipo_cbte = 6;                         // factura B
-    $tipo_doc = 80;                         // CUIT
-    $nro_doc = "20267565393";
-    $nombre_cliente = 'Mariano Reingart';
-    $domicilio_cliente = 'Balcarce 50';
-    $tipo_responsable = 5;                  // consumidor final
-    $referencia = "";                       // solo para NC / ND
-    
-    $ok = $ctrl->AbrirComprobante($tipo_cbte, $tipo_responsable, 
-                                  $tipo_doc, $nro_doc,
-                                  $nombre_cliente, $domicilio_cliente, 
-                                  $referencia);
-    echo "Abrir Comprobante = {$ok}\n";    
-    
-    # Imprimo un art�culo:
-    $codigo = "P0001";
-    $ds = "Descripcion del producto P0001";
-    $qty = 1.00;
-    $precio = 100.00;
-    $bonif = 0.00;
-    $alic_iva = 21.00;
-    $importe = 121.00;
-    $ok = $ctrl->ImprimirItem($ds, $qty, $importe, $alic_iva);
-    echo "ImprimirItem = {$ok}\n";
-
-    # Imprimir un pago (si es superior al total, se imprime el vuelto):
-    $ok = $ctrl->ImprimirPago("efectivo", $importe);
-    echo "ImprimirPago = {$ok}\n";
-    
-    # Finalizar el comprobante (imprime pie del comprobante, CF DGI, etc.) 
-    $ok = $ctrl->CerrarComprobante();
-    echo "CerrarComprobante = {$ok}\n";
-
-} catch (Exception $e) {
-    echo 'Excepci�n: ',  $e->getMessage(), "\n";
+  public function closingX() {
+    try {
+      if($this->ctrl->CierreDiario('X')) {
+        echo "<h3>Se realizó un cierre X!</h3>";
+      }
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+  }
 }
 
-echo "Finalizado\n";
+$env = parse_ini_file("env.ini");
+$invoice = new pyPrinter($env);
+
+if (isset($_GET['cierrez'])) {
+  $invoice->closingZ();
+} else if (isset($_GET['cierrex'])) {
+  $invoice->closingX();
+} else {
+  $invoice->receipt();
+}
+echo "<a href='./'>Volver</a>";
 ?>
